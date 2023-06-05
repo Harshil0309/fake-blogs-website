9a1eaef07e8fa3581d6f1558433cf256f9ae05be
Harshil0309 <harshilgupta562@gmail.com>
2023-06-05 23:39:41 +0530

    learning react

--- src/UserPage.jsx
+++ src/UserPage.jsx
@@ -7,12 +7,22 @@ import PostPage from "./PostPage";
 function UserPage() {
   const params = useParams();
   const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchuser = async () => {
-      const userdata = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${params.UserId}`
-      );
-      setUserData(userdata.data);
+      try {
+        const userdata = await axios.get(
+          `https://jsonplaceholder.typicode.com/users/${params.UserId}`
+        );
+        setUserData(userdata.data);
+      } catch (error) {
+        setError(error.message);
+        console.log(error);
+      }
+
+      setLoading(false);
+
       // console.log(userdata)
     };
     fetchuser();
@@ -22,13 +32,21 @@ function UserPage() {
 
   return (
     <div>
-      <h3>Id: {params.UserId}</h3>
-      <h3>Name: {userData.name}</h3>
-      <h3>UserName: {userData.username}</h3>
-      <h3>Email-Id: {userData.email}</h3>
-      <h3>Phone Number: {userData.phone}</h3>
-      <h3>Website: {userData.website}</h3>
-      <Link to={"/"}>CLick here to go back!!</Link>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div>
+          <h3>Id: {params.UserId}</h3>
+          <h3>Name: {userData.name}</h3>
+          <h3>UserName: {userData.username}</h3>
+          <h3>Email-Id: {userData.email}</h3>
+          <h3>Phone Number: {userData.phone}</h3>
+          <h3>Website: {userData.website}</h3>
+          <Link to={"/"}>CLick here to go back!!</Link>
+        </div>
+      )}
     </div>
   );
 }
